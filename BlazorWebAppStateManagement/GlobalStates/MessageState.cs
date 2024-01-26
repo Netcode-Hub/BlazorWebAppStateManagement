c1e46b360328e76f26791700000b15cc5acb5321
Frederick Asante <Rastinga1995@hotmail.com>
2024-01-26 10:23:08 +0000

    Add project files.

---
+++ BlazorWebAppStateManagement/GlobalStates/MessageState.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorWebAppStateManagement.GlobalStates
+{
+    public class MessageState
+    {
+        public string Message { get; set; }
+        public void SetMessage(string incomingText)
+        {
+            Message = incomingText;
+        }
+        public void ClearMessage()
+        {
+            Message = "";
+        }
+    }
+}
